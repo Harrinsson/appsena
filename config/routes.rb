23be8e9178252e6775a02e92769c900227dfe9c5
Harrinsson <charrisjack24@gmail.com>
2025-03-19 16:53:10 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  devise_for :user
+  root to: "home#welcome"
   get "up" => "rails/health#show", as: :rails_health_check
   get "carros/:id_carro" => "carros#carros", as: :carros
+  get "home" => "home#welcome", as: :welcome
   resources :motos
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
